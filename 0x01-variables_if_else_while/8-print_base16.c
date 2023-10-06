90f3b4179d3db016a9fdf7db685c85115a029c07
Dejiicodes <adewuniayodeji17@gmail.com>
2023-10-06 06:30:06 -0700

    commit for 5-9 pt.2

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible combinations of single-digit numbers.
+ * Numbers are separated by ',' followed by a space.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
