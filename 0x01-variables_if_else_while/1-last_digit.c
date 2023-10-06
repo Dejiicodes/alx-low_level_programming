97584afac2be9eb26c4470c37f3c840229ce5258
Dejiicodes <adewuniayodeji17@gmail.com>
2023-10-06 05:23:43 -0700

    9th commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,30 +12,30 @@
  */
 int main(void)
 {
-	int n;
-	int lastDigit;
+    int n;
+    int lastDigit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
 
-	/* Get the last digit of 'n' by taking the absolute value and modulo 10 */
-	lastDigit = abs(n) % 10;
+    /* Get the last digit of 'n' by taking the absolute value and modulo 10 */
+    lastDigit = abs(n) % 10;
 
-	printf("Last digit of %d is %d and is ", n, (n < 0) ? -lastDigit : lastDigit);
+    printf("Last digit of %d is %d and is ", n, (n < 0) ? -lastDigit : lastDigit);
 
-	if (lastDigit > 5)
-	{
-		printf("greater than 5\n");
-	}
-	else if (lastDigit == 0)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		printf("less than 6 and not 0\n");
-	}
+    if (lastDigit > 5)
+    {
+        printf("greater than 5\n");
+    }
+    else if (lastDigit == 0)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        printf("less than 6 and not 0\n");
+    }
 
-	return (0);
+    return (0);
 }
 
