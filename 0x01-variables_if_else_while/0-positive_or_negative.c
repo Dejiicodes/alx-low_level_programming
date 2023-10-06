ffa6994f643aca9819bde61cf8e833bef9d9e2fe
Dejiicodes <adewuniayodeji17@gmail.com>
2023-10-06 05:02:18 -0700

    4th commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,34 +6,34 @@
  * main - Entry point
  *
  * Description: This program generates a random number and
- *              prints whether it is positive, zero, or negative.
+ * prints whether it is positive, zero, or negative.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    /* Seed the random number generator */
-    srand(time(0));
+	/* Seed the random number generator */
+	srand(time(0));
 
-    /* Generate a random number and store it in 'n' */
-    n = rand() - RAND_MAX / 2;
+	/* Generate a random number and store it in 'n' */
+	n = rand() - RAND_MAX / 2;
 
-    /* Check if 'n' is positive, zero, or negative and print the result */
-    if (n > 0)
-    {
-        printf("%d is positive\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is zero\n", n);
-    }
-    else
-    {
-        printf("%d is negative\n", n);
-    }
+	/* Check if 'n' is positive, zero, or negative and print the result */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-    return (0);
+	return (0);
 }
 
