2713df361eac460615f22f9ec4f0e4590f640880
Dejiicodes <adewuniayodeji17@gmail.com>
2023-11-02 04:59:46 -0700

    Commit 7.0 for 0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,71 +1,126 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * is_digit - Check if a character is a digit.
- * @c: The character to check.
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
- * Return: 1 if the character is a digit, 0 otherwise.
+ * Return: no return.
  */
-int is_digit(char c) {
-    return c >= '0' && c <= '9';
+void _is_zero(char *argv[])
+{
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
 }
 
 /**
- * str_to_int - Convert a string to an integer.
- * @str: The string to convert.
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
  *
- * Return: The integer value of the string.
+ * Return: pointer of a char array.
  */
-int str_to_int(char *str) {
-    int result = 0;
-    int i;
-    for (i = 0; str[i] != '\0'; i++) {
-        result = result * 10 + (str[i] - '0');
-    }
-    return result;
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
 }
 
 /**
- * print_error - Print an error message and exit with status 98.
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
  */
-void print_error() {
-    fprintf(stderr, "Error\n");
-    exit(98);
+int _checknum(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
 }
 
-int main(int argc, char *argv[]) {
-    int len1, len2, i;
-    char *num1, *num2;
-    int result;
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
-    if (argc != 3) {
-        print_error();
-    }
-
-    num1 = argv[1];
-    num2 = argv[2];
-
-    len1 = strlen(num1);
-    len2 = strlen(num2);
-
-    for (i = 0; i < len1; i++) {
-        if (!is_digit(num1[i])) {
-            print_error();
-        }
-    }
-
-    for (i = 0; i < len2; i++) {
-        if (!is_digit(num2[i])) {
-            print_error();
-        }
-    }
-
-    result = str_to_int(num1) * str_to_int(num2);
-
-    printf("%d\n", result);
-
-    return 0;
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
+	{
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
+	}
+	printf("%s\n", nout);
+	return (0);
 }
-
